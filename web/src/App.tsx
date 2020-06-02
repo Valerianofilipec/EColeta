993848cda1bf86b6ec7e53852b73888f9ec845f1
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-06-02 17:05:59 +0100

    Mobile init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Esta App vai dar merda</h1>
+        <h3>Valeriano Filipe Calológio</h3>
       </header>
     </div>
   );
