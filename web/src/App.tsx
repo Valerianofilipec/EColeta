2a92491cb0fcbcd751f204c90206fbce226a2a4f
Valeriano Filipe Calológio <valeriofilipec@gmail.com>
2020-06-05 09:04:00 +0100

    Limpeza da Estrutura da App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Esta App vai dar merda</h1>
         <h3>Valeriano Filipe Calológio</h3>
       </header>
